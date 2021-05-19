ef0178cb6a5b076d437a229090bd1582ceb69294
Sayang Kamil5b <79760043+kamil5b@users.noreply.github.com>
2021-05-20 02:57:04 +0700

    Add files via upload

---
+++ test/test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kamil5b/golinkedlist"
+)
+
+func main() {
+	l := golinkedlist.CreateList()
+
+	l.Add(5)
+	l.Add(2)
+	l.Add(17)
+	l.Add(12)
+
+	for i := 0; i < l.Size; i++ {
+		val, _ := l.Get(i)
+		fmt.Println(val)
+	}
+	fmt.Println()
+
+	l.Sort(true)
+	for i := 0; i < l.Size; i++ {
+		val, _ := l.Get(i)
+		fmt.Println(val)
+	}
+
+}
